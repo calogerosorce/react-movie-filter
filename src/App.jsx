fb21f1bc0ddb2d434d17c5a7df02acb8e274c32b
calogerosorce <calogerosorce00@hotmail.com>
2025-11-11 13:37:30 +0100

    Add logic for selected the gener film

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Card from './components/Card'
 import Batman from './img/Batman.avif'
 import Inception from './img/Inception.png'
@@ -47,6 +47,25 @@ function App() {
     }
   ]
 
+  const [films, setFilms] = useState(film)
+  const [regola, setRegola] = useState(films)
+  const [select, setSelect] = useState('')
+
+  useEffect(() => {
+    const selectFilms = films.filter(items => items.genere.includes(select))
+    setRegola(selectFilms)
+
+    const selectElement = document.getElementById('floatingSelect');
+    const handleSelectChange = (event) => {
+      setSelect(event.target.value);
+    };
+    selectElement.addEventListener('change', handleSelectChange);
+
+    return () => {
+      selectElement.removeEventListener('change', handleSelectChange);
+    };
+
+  }, [films, select])
   return (
     <>
 
@@ -54,9 +73,18 @@ function App() {
       <main>
         <div className="content">
           <h1>Film</h1>
-          <div className="row g-4 p-5">
-            {film.map(items => (
-              <Card src={items.src} title={items.title} genere={items.genere} />
+          <div className="row g-4 m-3">
+            <div className="form-floating">
+              <select className="form-select bg-dark text-white" id="floatingSelect" aria-label="Floating label select example">
+                <option value={''}>Open this select menu</option>
+                <option value="Fantascienza">Fantascienza</option>
+                <option value="Thriller">Thriller</option>
+                <option value="Romantico">Romantico</option>
+                <option value="Azione">Azione</option>
+              </select>
+            </div>
+            {regola.map(items => (
+              <Card key={items.id} src={items.src} title={items.title} genere={items.genere} />
             ))}
           </div>
         </div>
