ff89536688511ef04c31c2734cecd6c777cb800c
calogerosorce <calogerosorce00@hotmail.com>
2025-11-11 17:02:18 +0100

    Create the logic for add new card

--- src/App.jsx
+++ src/App.jsx
@@ -51,6 +51,11 @@ function App() {
   const [regola, setRegola] = useState(films)
   const [select, setSelect] = useState('')
   const [search, setSearch] = useState('')
+  const [title, setTitle] = useState('')
+  const [image, setImage] = useState('')
+  const [genere, setGenere] = useState('')
+
+
 
   useEffect(() => {
     const selectFilms = films.filter(items => items.genere.includes(select))
@@ -83,6 +88,23 @@ function App() {
 
   }
 
+  function handleSubmitAdd(e) {
+    e.preventDefault()
+    if (regola.length > 1) {
+      let newId = regola[regola.length - 1]?.id + 1
+      const newArray = [...regola, { id: newId, title: title, genere: genere, src: image }]
+      setRegola(newArray)
+
+    }
+  }
+
+  const newArray = []
+  for (let i = 0; i < regola.length; i++) {
+    const items = regola[i].genere
+    if (!newArray.includes(items)) {
+      newArray.push(items)
+    }
+  }
   return (
     <>
 
@@ -93,21 +115,30 @@ function App() {
           <div className="row g-4 m-3">
             <form onSubmit={handleSubmit}>
               <div className="input-group mb-3">
-                <input type="text" className="form-control bg-dark border rounded-start border-secondary px-2" placeholder="Search film" aria-label="Search film" aria-describedby="search_film" value={search} onChange={(e) => setSearch(e.target.value)} />
+                <input type="text" className="form-control text-white bg-dark border rounded-start border-secondary px-2" placeholder="Search film" aria-label="Search film" aria-describedby="search_film" value={search} onChange={(e) => setSearch(e.target.value)} />
                 <button className="btn btn-outline-secondary" type="submit" id="search_film" >Button</button>
               </div>
             </form>
             <div className="form-floating">
               <select className="form-control bg-dark text-secondary border border-secondary" id="floatingSelect" aria-label="Floating label select example">
                 <option value={''}>Open this select menu</option>
-                <option value="Fantascienza">Fantascienza</option>
-                <option value="Thriller">Thriller</option>
-                <option value="Azione">Azione</option>
+                {newArray.map((items, i) => (
+                  <option key={i} value={items}>{items}</option>
+                ))}
               </select>
             </div>
             {regola.map(items => (
               <Card key={items.id} src={items.src} title={items.title} genere={items.genere} />
             ))}
+            <form id="form" onSubmit={handleSubmitAdd}>
+              <label className="form-label text-white">Title</label>
+              <input type="text" id="inputTitle" className="form-control text-white bg-dark border rounded-start border-secondary px-2" aria-describedby="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+              <label className="form-label text-white">Genere</label>
+              <input type="text" id="inputGenere" className="form-control text-white bg-dark border rounded-start border-secondary px-2" aria-describedby="genere" value={genere} onChange={(e) => setGenere(e.target.value)} />
+              <label className="form-label text-white">Image</label>
+              <input type="text" id="inputImage" className="form-control text-white bg-dark border rounded-start border-secondary px-2" aria-describedby="image" value={image} onChange={(e) => setImage(e.target.value)} />
+              <button type="submit" className="btn btn-primary my-3">Confirm identity</button>
+            </form>
           </div>
         </div>
       </main>
